1b305922a2337c0fbf741f0f42e08eb8977b1654
GRADEN BISHOP <4400112995@OTECH.EDU>
2022-02-14 13:51:36 -0700

    complete

--- A2-5-Output/Program.cs
+++ A2-5-Output/Program.cs
@@ -6,7 +6,21 @@ namespace A2_5_Output
     {
         static void Main()
         {
-            Console.WriteLine("tomato");
+            string one = "1";
+            string two = "2";
+            string three = "3";
+            string four = "4";
+
+            Console.WriteLine("1 2 3 4");
+           // Console.WriteLine("");
+            Console.Write("1 ");
+            Console.Write("2 ");
+            Console.Write("3 ");
+            Console.Write("4 ");
+            Console.WriteLine("");
+            Console.WriteLine($"{one} {two} {three} {four}");
+
+
         }
     }
 }
